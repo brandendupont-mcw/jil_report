d1d34bb0349147a647288e8d5504dbf9d1a57e4b
brandendupont-mcw <bdupont@mcw.edu>
2022-05-15 02:00:42 -0400

    update

---
+++ components/comments/Background.js
@@ -0,0 +1,9 @@
+export default function Background({ children }) {
+    return <div className="mb-4">
+        <div className="bg-gray-100 px-4 pt-1 pb-1">
+            <h4> What does this mean for those arrested? </h4>
+            {children}
+        </div>
+    </div>
+  }
+  
