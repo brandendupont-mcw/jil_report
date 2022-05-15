ed9a96bc447ec42d3698ced66d6d7878a2a2526c
brandendupont-mcw <bdupont@mcw.edu>
2022-05-15 16:10:55 -0400

    lets go boys

---
+++ components/feature.js
@@ -0,0 +1,37 @@
+import React from "react";
+import Link from './Link'
+
+
+
+const Highlight7 = () => {
+    return (
+        <div className="2xl:container 2xl:mx-auto lg:px-20 md:py-12 md:px-6 py-1 px-4 bg-hero mb-24">
+            <div className=" grid lg:grid-cols-2 md:grid-cols-2 grid-cols-1 lg:gap-20 gap-6 ">
+                {/* Safe Shopping Grid Card */}
+                <div className=" p-6 bg-gray-50 shadow-xs hover:shadow-lg hover:border-r-[9px] hover:border-b-[9px] border-black border-2 rounded-lg transition duration-300 ease-in-out hover:-translate-y-1 hover:translate-x-1  ">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+  <path strokeLinecap="round" strokeLinejoin="round" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
+</svg>
+                    <p className=" text-xl text-black font-semibold leading-5 mt-6">Data Tools</p>
+                    <p className=" font-normal text-base leading-6 text-gray-600 my-4">Interactive visualizations to explore PFA changes</p>
+                    <Link href="" className=" cursor-pointer text-base leading-4 font-medium text-gray-800 border-b-2 border-gray-800 hover:border-maroon hover:text-maroon ">View here</Link>
+                </div>
+
+                {/* Personal Shopping Grid Card */}
+
+
+                {/* Free Shopping Grid Card */}
+                <div className=" p-6 bg-gray-50 shadow-xs hover:shadow-lg hover:border-r-[9px] hover:border-b-[9px] border-black border-2 rounded-lg transition duration-300 ease-in-out hover:-translate-y-1 hover:translate-x-1 ">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+  <path strokeLinecap="round" strokeLinejoin="round" d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" />
+</svg>
+                    <p className=" text-xl text-gray-800 font-semibold leading-5 mt-6">Data Stories</p>
+                    <p className=" font-normal text-base leading-6 text-gray-600 my-4">Data-driven stories investigating the PFA's impact </p>
+                    <Link href="/blog"  className=" cursor-pointer text-base leading-4 font-medium text-gray-800 border-b-2 border-gray-800 hover:border-maroon hover:text-maroon ">View here</Link>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Highlight7;
--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import siteMetadata from '@/data/siteMetadata'
 import { getAllFilesFrontMatter } from '@/lib/mdx'
 import formatDate from '@/lib/utils/formatDate'
 import Hero from '@/components/Hero'
+import Feature from '@/components/feature'
 
 import NewsletterForm from '@/components/NewsletterForm'
 
@@ -21,6 +22,7 @@ export default function Home({ posts }) {
     <>
       <PageSEO title={siteMetadata.title} description={siteMetadata.description} />
       <Hero />
+      <Feature />
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pt-6 pb-8 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-3xl md:leading-14">
